6215b465418e49699130a5871b758347d39545d4
Omar <Omar@OMAR-PC>
2018-10-30 15:05:55 +0200

    Login Process Step to Tidal With Artists Searches

--- OmarApp/Program.cs
+++ OmarApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using OpenTidl.Methods;
+using OpenTidl.Models;
+using OpenTidl.Models.Base;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -16,10 +18,27 @@ namespace OmarApp
             OpenTidl.OpenTidlClient openTidlClient = new OpenTidl.OpenTidlClient(clientConfiguration);
             Task<OpenTidlSession> loginSession = openTidlClient.LoginWithUsername("omar13489@gmail.com", "Zubur123!");
             Console.WriteLine("Waiting Until the Login Complete...");
-            Debug.WriteLine("Waiting Until the Login Complete...");
             loginSession.Wait();
-            Console.WriteLine("The Login Process had been ended succesfully with this Login Session " + loginSession.Result.);
-            Debug.WriteLine("The Login Process had been ended succesfully with this Login Session " + loginSession.Result);
+            Console.WriteLine("The Login Process had been ended succesfully with this User Info : \n" 
+                + "Date Of Birth : " + loginSession.Result.GetUser().Result.DateOfBirth + "\n" 
+                + "First Name : " + loginSession.Result.GetUser().Result.FirstName + "\n"
+                + "Last Name : " + loginSession.Result.GetUser().Result.LastName + "\n"
+                + "Gender : " + loginSession.Result.GetUser().Result.Gender + "\n"
+                + "Email : " + loginSession.Result.GetUser().Result.Email + "\n"
+                + "Username : " + loginSession.Result.GetUser().Result.Username + "\n");
+
+            Task<JsonList<ArtistModel>> artistsResults = openTidlClient.SearchArtists("Nancy Ajram", 0, 9999);
+            
+            foreach (var artistModel in artistsResults.Result.Items)
+            {
+                Console.WriteLine("The Artist Model ID : " + artistModel.Id);
+                Console.WriteLine("The Artist Model Name : " + artistModel.Name);
+                Console.WriteLine("The Artist Model URL : " + artistModel.Url + "\n");
+            }
+
+            Console.WriteLine("Log Out this user : " + loginSession.Result.GetUser().Result.Username);
+            Task<EmptyModel> logoutResult = loginSession.Result.Logout();
+            logoutResult.Wait();
             Console.ReadLine();
         }
     }
