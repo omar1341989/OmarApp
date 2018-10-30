b1683b6b1b9c69bf73252ed397f0585fc521177c
Omar <Omar@OMAR-PC>
2018-10-30 16:50:35 +0200

    New Modifications, Add Wait Function for each asynchronus Task

--- OmarApp/Program.cs
+++ OmarApp/Program.cs
@@ -28,15 +28,22 @@ namespace OmarApp
                 + "Username : " + loginSession.Result.GetUser().Result.Username + "\n");
 
             Task<JsonList<ArtistModel>> artistsResults = openTidlClient.SearchArtists("Nancy Ajram", 0, 4);
-            
+
+            Console.WriteLine("Wait until all the artists had been fetched correctly!");
+            artistsResults.Wait();
+
             foreach (var artistModel in artistsResults.Result.Items)
             {
                 Console.WriteLine("The Artist Model ID : " + artistModel.Id);
                 Console.WriteLine("The Artist Model Name : " + artistModel.Name);
                 Console.WriteLine("The Artist Model URL : " + artistModel.Url + "\n");
 
-                Task<JsonList<AlbumModel>> artistAlbumsObject = openTidlClient.GetArtistAlbums(artistModel.Id,
-                                                                              OpenTidl.Enums.AlbumFilter.ALBUMS,0,200);
+                Task<JsonList<AlbumModel>> artistAlbumsObject = 
+                openTidlClient.GetArtistAlbums(artistModel.Id,
+                                               OpenTidl.Enums.AlbumFilter.ALBUMS,0,200);
+                Console.WriteLine("Wait until all the albums for the specific artist had been fetched correctly!");
+                artistAlbumsObject.Wait();
+
                 Console.WriteLine("Albums for " + artistModel.Name);
                 foreach (var albumModelObject in artistAlbumsObject.Result.Items)
                 {
@@ -47,6 +54,7 @@ namespace OmarApp
                     Console.WriteLine("\t Album Number of Tracks : " + albumModelObject.NumberOfTracks);
                     Console.WriteLine("\t Album Number of Volumes : " + albumModelObject.NumberOfVolumes);
                     Console.WriteLine("\t Album Release Date : " + albumModelObject.ReleaseDate);
+                    Console.WriteLine("\t Album Stream Start Date: " + albumModelObject.StreamStartDate);
                     Console.WriteLine("\t Album URL : " + albumModelObject.Url + "\n");
                 }
             }
