06e80e60fe90204a8c4971d30edc625ab500cb1b
Omar <Omar@OMAR-PC>
2018-10-30 13:49:49 +0200

    The Login Process to Tidol

--- OmarApp/Program.cs
+++ OmarApp/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OpenTidl.Methods;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,15 @@ namespace OmarApp
     {
         static void Main(string[] args)
         {
+            OpenTidl.ClientConfiguration clientConfiguration = OpenTidl.ClientConfiguration.Default;
+            OpenTidl.OpenTidlClient openTidlClient = new OpenTidl.OpenTidlClient(clientConfiguration);
+            Task<OpenTidlSession> loginSession = openTidlClient.LoginWithUsername("omar13489@gmail.com", "Zubur123!");
+            Console.WriteLine("Waiting Until the Login Complete...");
+            Debug.WriteLine("Waiting Until the Login Complete...");
+            loginSession.Wait();
+            Console.WriteLine("The Login Process had been ended succesfully with this Login Session " + loginSession.Result.);
+            Debug.WriteLine("The Login Process had been ended succesfully with this Login Session " + loginSession.Result);
+            Console.ReadLine();
         }
     }
 }
